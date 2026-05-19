16c0e9aaf4d2463f674a8010a30a25d5bf96c957
MartinGruter <Martin.Grter@iths.se>
2026-05-19 10:06:26 +0200

    Initial commit

---
+++ src/main/java/se/iths/martin/authserverprojekt2/AuthServerProjekt2Application.java
@@ -0,0 +1,13 @@
+package se.iths.martin.authserverprojekt2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServerProjekt2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServerProjekt2Application.class, args);
+	}
+
+}
---
+++ src/test/java/se/iths/martin/authserverprojekt2/AuthServerProjekt2ApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.martin.authserverprojekt2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerProjekt2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
